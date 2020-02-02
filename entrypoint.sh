55286b648087d3cad5f5fb8827ffd3c7cabb46c1
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-02 16:05:00 +0000

    Added spaces.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "$GITHUB_WORKSPACE"
 
 ln -s /clang-format/clang-format${INPUT_CLANGFORMATVERSION} /usr/bin/clang-format
 
-echo /run-clang-format.py -r --cli-args="${INPUT_CLIARGS}" --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS} ${INPUT_SOURCE}
-/run-clang-format.py -r --cli-args="${INPUT_CLIARGS}" --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS} ${INPUT_SOURCE}
+echo /run-clang-format.py -r --cli-args=\"${INPUT_CLIARGS}\" --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS} ${INPUT_SOURCE}
+/run-clang-format.py -r --cli-args=\"${INPUT_CLIARGS}\" --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS} ${INPUT_SOURCE}
